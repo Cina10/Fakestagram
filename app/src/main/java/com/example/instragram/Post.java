b0cc46e48508ab45418878a98309ea1e5afe1a18
Chianna Cohen <chianna@fb.com>
2020-07-10 13:56:17 -0700

    new details activity

--- app/src/main/java/com/example/instragram/Post.java
+++ app/src/main/java/com/example/instragram/Post.java
@@ -1,16 +1,31 @@
 package com.example.instragram;
 
+import android.util.Log;
+
 import com.parse.ParseClassName;
 import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 @ParseClassName("Post")
 public class Post extends ParseObject {
+    public static final String TAG = "Post";
     public static final String KEY_DESCRIPTION = "description";
     public static final String KEY_IMAGE = "image";
     public static final String KEY_USER = "user";
     public static final String CREATED_AT = "createdAt";
+
+    //for time calculations
+    private static final int SECOND_MILLIS = 1000;
+    private static final int MINUTE_MILLIS = 60 * SECOND_MILLIS;
+    private static final int HOUR_MILLIS = 60 * MINUTE_MILLIS;
+    private static final int DAY_MILLIS = 24 * HOUR_MILLIS;
+    private static final int YEAR_MILLIS = 365 * DAY_MILLIS;
 
     public String getDescription() {
         return getString(KEY_DESCRIPTION);
@@ -35,4 +50,37 @@ public class Post extends ParseObject {
     public void setParseUser(ParseUser user) {
         put(KEY_USER, user);
     }
+
+    public Date getCreatedAt() {
+        return getDate(CREATED_AT);
+    }
+
+    public String getRelativeTime(Date createdAt) {
+
+        long time = getCreatedAt().getTime();
+        long now = System.currentTimeMillis();
+        final long diff = now - time;
+        if (diff < MINUTE_MILLIS) {
+            return "just now";
+        } else if (diff < 2 * MINUTE_MILLIS) {
+            return "a minute ago";
+        } else if (diff < 60 * MINUTE_MILLIS) {
+            return diff / MINUTE_MILLIS + " minutes ago";
+        } else if (diff < 90 * MINUTE_MILLIS) {
+            return "an hour ago";
+        } else if (diff < 24 * HOUR_MILLIS) {
+            return diff / HOUR_MILLIS + " hours ago";
+        } else if (diff < 2 * DAY_MILLIS) {
+            return "1 day ago";
+        } else if (diff < 7 * DAY_MILLIS) {
+            return diff / DAY_MILLIS + " days ago";
+        } else if (diff < YEAR_MILLIS) {
+            return String.format("%tB %td", createdAt);
+        } else {
+            // display time and date
+            return String.format("%tB %td, %tY", createdAt);
+        }
+    }
 }
+
+
